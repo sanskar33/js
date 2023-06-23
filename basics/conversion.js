e02b6b3b091807b341c6eea3e5dc7936fcca8006
sanskar33 <66031146+sanskar33@users.noreply.github.com>
2023-06-23 10:34:55 +0000

    changes update

--- basics/conversion.js
+++ basics/conversion.js
@@ -48,15 +48,15 @@ let str2 = " hitesh"
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2);  // 12
+// console.log(1 + "2");  // 12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); // 32
 
-// console.log( (3 + 4) * 5 % 3);
+// console.log( (3 + 4) * 5 % 3); //2
 
-// console.log(+true);
-// console.log(+"");
+// console.log(+true); // 1
+// console.log(+"");  //  /0
 
 let num1, num2, num3
 
@@ -64,7 +64,7 @@ num1 = num2 = num3 = 2 + 2
 
 let gameCounter = 100
 ++gameCounter;
-console.log(gameCounter);
+console.log(gameCounter); //101
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
